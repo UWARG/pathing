58032041d0f95edf8d63f70c8112b30fb48abc51
wuda17 <dwxcan@gmail.com>
2023-07-10 10:15:00 -0400

    cleanup and test

--- modules/waypoint_names_to_coordinates.py
+++ modules/waypoint_names_to_coordinates.py
@@ -1,5 +1,5 @@
 """
-Method to convert list of waypoints to dronekit commands
+Function to convert list of waypoints to dronekit commands
 """
 import dronekit
 
@@ -44,3 +44,20 @@ def waypoints_to_commands(waypoints: "list[tuple[float, float]]",
         dronekit_command_list.append(command)
 
     return dronekit_command_list
+
+
+# short test to ensure proper functionality, can delete if necessary or create proper unittest
+if __name__ == '__main__':
+    # generate test data
+    test_waypoint_data = []
+    test_altitude = 40
+    for i in range(10):
+        sample_waypoint = (-40, 84)
+        test_waypoint_data.append(sample_waypoint)
+
+    # make function call
+    test_received_commands = waypoints_to_commands(test_waypoint_data, test_altitude)
+
+    # verify function output
+    for received_command in test_received_commands:
+        print(received_command)
