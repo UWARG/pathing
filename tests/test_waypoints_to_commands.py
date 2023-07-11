1ddd0433514996742f03e968cde3dc3f510b8d02
David Wu <84358223+wuda17@users.noreply.github.com>
2023-07-10 23:07:40 -0400

    Convert waypoint coordinates to commands (#6)
    * initial commit
    
    * cleanup and test
    
    * filename
    
    * created tests and pr changes
    
    * pr changes global constants and test improvemets
    
    * final nit cleanup
    
    * final doc

---
+++ tests/test_waypoints_to_commands.py
@@ -0,0 +1,35 @@
+"""
+Test process
+"""
+
+import dronekit
+
+from modules import waypoints_to_commands
+
+
+def test_waypoints_to_commands():
+    """
+    Tests functionality correctness of waypoints_to_commands
+    """
+    waypoints = [(42.123, -73.456), (42.789, -73.987), (42.555, -73.321)]
+    altitude = 100
+
+    commands_actual = waypoints_to_commands.waypoints_to_commands(waypoints, altitude)
+
+    assert isinstance(commands_actual, list)
+    assert len(commands_actual) == len(waypoints)
+
+    for i, command in enumerate(commands_actual):
+        lat_expected = waypoints[i][0]
+        lng_expected = waypoints[i][1]
+
+        assert isinstance(command, dronekit.Command)
+        assert command.frame == waypoints_to_commands.MAVLINK_FRAME
+        assert command.command == waypoints_to_commands.MAVLINK_COMMAND
+        assert command.param1 == 0
+        assert command.param2 == waypoints_to_commands.ACCEPT_RADIUS
+        assert command.param3 == 0
+        assert command.param4 == 0
+        assert command.x == lat_expected
+        assert command.y == lng_expected
+        assert command.z == altitude
