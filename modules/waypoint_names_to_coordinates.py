d4a4ea342a134636764d8567d18601d54b796c92
Nikunj Patel <96856377+nrrpatel@users.noreply.github.com>
2023-07-12 19:30:26 -0400

    fixed  comments and created a new test file

--- modules/waypoint_names_to_coordinates.py
+++ modules/waypoint_names_to_coordinates.py
@@ -1,59 +1,31 @@
-def waypoint_names_to_coordinates(waypoint_names: list[str],
-                                  waypoint_mapping: dict[str, tuple[float, float]]) \
-        -> tuple[bool, list[tuple[float, float]]]:
+"""
+This file contains a function for converting waypoint names to coordinates.
+"""
+
+def waypoint_names_to_coordinates(waypoint_names: "list[str]",
+                                  waypoint_mapping: "dict[str, tuple[float, float]]") \
+        -> "tuple[bool, list[tuple[float, float]]]":
+    """
+    Converts a list of waypoint names to their corresponding coordinates based on a waypoint mapping.
+
+    Args:
+        waypoint_names (list[str]): A list of waypoint names.
+        waypoint_mapping (dict[str, tuple[float, float]]): A dictionary mapping waypoint names to their coordinates.
+
+    Returns:
+        tuple[bool, list[tuple[float, float]]]: A tuple containing a boolean indicating the success of the conversion
+            and a list of coordinates corresponding to the given waypoint names.
+    """
     coordinates = []
+
+    if not waypoint_names:  # Handle the case when waypoint_names is empty
+        return True, coordinates
+
     for name in waypoint_names:
         if name in waypoint_mapping:
-            coordinates.append(waypoint_mapping[name])
+            coordinate = waypoint_mapping[name]
+            coordinates.append(coordinate)
         else:
             return False, None
+
     return True, coordinates
-
-names_valid = ["Waterloo", "Aerial", "Robotics", "Group 15"]
-waypoint_dictionary = {
-    "Aerial": (9, 7),
-    "Group 15": (3, 4),
-    "Robotics": (-1, 0),
-    "University of Waterloo Station for 301 ION": (6, 6),
-    "WARG": (8, 2),
-    "Waterloo": (2, -5),
-    "Alpha": (),
-    "Bravo": (),
-    "Charlie": (),
-    "Delta": (),
-    "Echo": (),
-    "Foxtrot": (),
-    "Golf": (),
-    "Hotel": (),
-    "India": (),
-    "Juliette": (),
-    "Kilo": (),
-    "Lima": (),
-    "Mike": (),
-    "November": (),
-    "Oscar": (),
-    "Papa": (),
-    "Quebec": (),
-    "Point 18": (),
-    "Romeo": (),
-    "Sierra": (),
-    "Tango": (),
-    "Uniform": (),
-    "Victor": (),
-    "Whiskey": (),
-    "Xray": (),
-    "Yankee": (),
-    "Zulu": (),
-#Added all the waypoint names so it can be used later with the coordinates
-
-}
-
-result, value = waypoint_names_to_coordinates(names_valid, waypoint_dictionary)
-print(result)  # True
-print(value)  # [(2, -5), (9, 7), (-1, 0), (3, 4)]
-
-names_invalid = ["WARG", "Hello", "World"]
-
-result, value = waypoint_names_to_coordinates(names_invalid, waypoint_dictionary)
-print(result)  # False
-print(value)  # None
---
+++ tests/test_waypoints_names_to_coordinates.py
@@ -0,0 +1,46 @@
+
+names_valid = ["Waterloo", "Aerial", "Robotics", "Group 15"]
+waypoint_dictionary = {
+    "Aerial": (9, 7),
+    "Group 15": (3, 4),
+    "Robotics": (-1, 0),
+    "University of Waterloo Station for 301 ION": (6, 6),
+    "WARG": (8, 2),
+    "Waterloo": (2, -5),
+    "Alpha": (),
+    "Bravo": (),
+    "Charlie": (),
+    "Delta": (),
+    "Echo": (),
+    "Foxtrot": (),
+    "Golf": (),
+    "Hotel": (),
+    "India": (),
+    "Juliette": (),
+    "Kilo": (),
+    "Lima": (),
+    "Mike": (),
+    "November": (),
+    "Oscar": (),
+    "Papa": (),
+    "Quebec": (),
+    "Point 18": (),
+    "Romeo": (),
+    "Sierra": (),
+    "Tango": (),
+    "Uniform": (),
+    "Victor": (),
+    "Whiskey": (),
+    "Xray": (),
+    "Yankee": (),
+    "Zulu": (),
+}
+
+result, value = waypoint_names_to_coordinates(names_valid, waypoint_dictionary)
+print(result)  # True
+print(value)  # [(2, -5), (9, 7), (-1, 0), (3, 4)]
+
+names_empty = []
+result, value = waypoint_names_to_coordinates(names_empty, waypoint_dictionary)
+print(result)  # True
+print(value)  # []
