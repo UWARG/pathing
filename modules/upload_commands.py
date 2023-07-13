de5adcd2ca8fde8c2524f2bf1f6ec1882998ba8e
ArVMal <87793211+ArVMal@users.noreply.github.com>
2023-07-13 17:06:58 -0400

    1st update

--- modules/upload_commands.py
+++ modules/upload_commands.py
@@ -1,7 +1,6 @@
 """
-Function to upload dronekit commands
+Function to upload dronekit commands.
 """
-
 
 import dronekit
 
@@ -9,32 +8,34 @@ import dronekit
 def upload_commands(drone: dronekit.Vehicle, commands: "list[dronekit.Command]") -> None:
     """
     Add the list of commands to the drone’s command sequence, and upload them.
+    If the list is empty, does not upload anything.
 
     Parameters
     -----------
     drone: dronekit.Vehicle
-        the connected drone.
+        The connected drone.
     commands: list[dronekit.Command]
-        list of dronekit commands.
+        List of dronekit commands.
 
     Returns
     -------
     None
     """
-    # Get the set of commands from the drone
-    command_sequence = drone.commands
-    command_sequence.download()
-    command_sequence.wait_ready()
-    command_sequence.clear()
-    for command in commands:
-        command_sequence.add(command)
+    # If the list is empty, do nothing
+    if (len(commands) == 0):
+        pass
+    else:    
+        # Download the command sequence and clear it
+        # This is to avoid duplicate or conflicting commands
+        command_sequence = drone.commands
+        command_sequence.download()
+        command_sequence.wait_ready()
+        command_sequence.clear()
 
-    # Upload the commands to drone
-    command_sequence.upload()
-    
+        # Adds new commands to command sequence
+        for command in commands:
+            command_sequence.add(command)
 
-
-
-
-
-    
+        # Upload the commands to drone
+        command_sequence.upload()
+        
