822d308e9dcd157b6142cf516336854222833c79
Dylan Finlay <dylan.finlay33@gmail.com>
2023-09-26 20:17:15 -0400

    Dictionary replaced with list of Waypoint objects

--- modules/load_waypoint_name_to_coordinates_map.py
+++ modules/load_waypoint_name_to_coordinates_map.py
@@ -3,16 +3,19 @@ Name-coordinate mapping from CSV file.
 """
 import pathlib
 
+from modules.waypoint_class import Waypoint
+
+# Function to create a list of all Waypoint objects from a CSV file
 
 def load_waypoint_name_to_coordinates_map(waypoint_file_path: pathlib.Path) \
-    -> "tuple[bool, dict[str, tuple[float, float]]]":
+    -> "tuple[bool, list[Waypoint]]":
     """
-    Creates a name to coordinate dictionary from the CSV file.
+    Creates a list of Waypoint ojects with names and coordinates from the CSV file.
     """
     if not waypoint_file_path.exists():
         return False, None
 
-    name_to_coordinates_map = {}
+    waypoints_map = []
     with open(waypoint_file_path, encoding="utf-8") as file:
         for line in file:
             # Skip header and empty lines
@@ -20,9 +23,37 @@ def load_waypoint_name_to_coordinates_map(waypoint_file_path: pathlib.Path) \
                 continue
 
             name, latitude, longitude = line.split(',')
-            name_to_coordinates_map[name] = (float(latitude), float(longitude))
+            waypoint = Waypoint(name, float(latitude), float(longitude))
+            waypoints_map.append(waypoint)
 
-    if len(name_to_coordinates_map) > 0:
-        return True, name_to_coordinates_map
+    if len(waypoints_map) > 0:
+        return True, waypoints_map
 
     return False, None
+
+# # Original code
+
+# def load_waypoint_name_to_coordinates_map(waypoint_file_path: pathlib.Path) \
+#     -> "tuple[bool, dict[str, tuple[float, float]]]":
+#     """
+#     Creates a name to coordinate dictionary from the CSV file.
+#     """
+#     if not waypoint_file_path.exists():
+#         return False, None
+
+#     name_to_coordinates_map = {}
+#     with open(waypoint_file_path, encoding="utf-8") as file:
+#         for line in file:
+#             # Skip header and empty lines
+#             if line in ("name,latitude,longitude\n", ""):
+#                 continue
+
+#             name, latitude, longitude = line.split(',')
+#             name_to_coordinates_map[name] = (float(latitude), float(longitude))
+
+#     if len(name_to_coordinates_map) > 0:
+#         return True, name_to_coordinates_map
+
+#     return False, None
+
+
