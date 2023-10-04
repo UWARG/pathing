5a05c659681d16320382ab14f83348afb6796f61
roskzhu <roskenzhu@gmail.com>
2023-10-03 21:36:24 -0400

    reverted prev change for merge

--- path_2023.py
+++ path_2023.py
@@ -20,7 +20,7 @@ ALTITUDE = 40
 CONNECTION_ADDRESS = "tcp:localhost:14550"
 
 def run() -> int:
-    drone = dronekit.connect(CONNECTION_ADDRESS, wait_ready = True, timeout=60)
+    drone = dronekit.connect(CONNECTION_ADDRESS, wait_ready = True)
 
     result, waypoint_name_to_coordinates = load_waypoint_name_to_coordinates_map.load_waypoint_name_to_coordinates_map(WAYPOINT_FILE_PATH)
     if not result:
