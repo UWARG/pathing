0357a1a17c54948555ca28250546b5c03bf0726c
ArVMal <87793211+ArVMal@users.noreply.github.com>
2023-10-10 18:43:35 -0400

    Update test_diversion_qr_to_waypoint_list.py

--- tests/test_diversion_qr_to_waypoint_list.py
+++ tests/test_diversion_qr_to_waypoint_list.py
@@ -9,11 +9,15 @@ def test_valid_multiple_avoid_waypoints():
     """
     Test valid input.
     """
+    # Setup
     input = "Avoid the area bounded by: Alpha; Beta. Rejoin the route at Gamma"
-    expected = (True, (["Alpha", "Beta"], "Gamma"))
+    expected = ["Alpha", "Beta"], "Gamma"
 
-    actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
+    # Run
+    result, actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
 
+    # Test
+    assert result
     assert actual == expected
 
 
@@ -21,11 +25,15 @@ def test_valid_single_avoid_waypoint():
     """
     Test valid input of single waypoint list and a rejoin waypoint.
     """
+    # Setup
     input = "Avoid the area bounded by: Alpha. Rejoin the route at Beta"
-    expected = (True, (["Alpha"], "Beta"))
+    expected = ["Alpha"], "Beta"
 
-    actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
+    # Run
+    result, actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
 
+    # Test
+    assert result
     assert actual == expected
 
 
@@ -33,58 +41,74 @@ def test_valid_waypoints_whitespace():
     """
     Test valid input with varying amounts of whitespace in between.
     """
+    # Setup
     input = "Avoid the area bounded by: A;   B; C,;    D     . Rejoin the route at  Beta   "
-    expected = (True, (["A", "B", "C,", "D"], "Beta"))
+    expected = ["A", "B", "C,", "D"], "Beta"
 
-    actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
+    # Run
+    result, actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
 
+    # Test
+    assert result
     assert actual == expected
-    
+
 
 def test_empty_waypoint_list_and_rejoin():
     """
     Test input without waypoints list and rejoin waypoint.
     """
+    # Setup
     input = "Avoid the area bounded by: . Rejoin the route at"
-    expected = False, None
 
-    actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
+    # Run
+    result, actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
 
-    assert actual == expected
+    # Test
+    assert not result
+    assert actual is None
 
 
 def test_empty_waypoint_list():
     """
     Test case without a waypoint list.
     """
+    # Setup
     input = "Avoid the area bounded by: ; ;   ;   ;. Rejoin the route at Beta"
-    expected = False, None
 
-    actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
+    # Run
+    result, actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
 
-    assert actual == expected
+    # Test
+    assert not result
+    assert actual is None
 
 
 def test_empty_rejoin_waypoint():
     """
     Test case without a rejoin waypoint.
     """
+    # Setup
     input = "Avoid the area bounded by: Zulu; Bravo; Tango; Uniform. Rejoin the route at"
-    expected = False, None
 
-    actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
+    # Run
+    result, actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
 
-    assert actual == expected
+    # Test
+    assert not result
+    assert actual is None
 
 
 def test_mismatch_input():
     """
     Test case with no matching text
     """
+    # Setup
     input = "Lorem ipsum dolor sit amet. Zulu; Tango; Alpha."
-    expected = False, None
 
-    actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
+    # Run
+    result, actual = diversion_qr_to_waypoint_list.diversion_qr_to_waypoint_list(input)
 
-    assert actual == expected
+    # Test
+    assert not result
+    assert actual is None
 
