46d06aec115882b402f478bb6e351033934b5c5f
eshaan-mehta <eshaanm.mehta@gmail.com>
2023-10-24 23:29:03 -0400

    PR 2

--- modules/waypoints_dict_to_list.py
+++ modules/waypoints_dict_to_list.py
@@ -1,5 +1,5 @@
 """
-Module to convert waypoints dict to waypoints list, and generate kml file.
+Module to convert waypoints dict to waypoints list.
 """
 
 def waypoints_dict_to_list(waypoint_name_to_coordinates: "dict[str, tuple[float, float]]") -> "tuple[bool, list[tuple[float,float]]]":
@@ -9,14 +9,13 @@ def waypoints_dict_to_list(waypoint_name_to_coordinates: "dict[str, tuple[float,
     Parameters
     ----------
     waypoint_name_to_coordinates: dict[str, tuple[float, float]]]
-        Waypoints in key: value form, "waypoint name": (latitude, longitude).
+        Waypoint mapping of name to (latitude, longitude).
 
     Returns
     -------
     bool: Whether waypoint data conversion was a success.
-    list[tuple[float, float]]: list of the waypoint coordinates
+    list[tuple[float, float]]: List of the waypoint coordinates.
     """
-
     # check for empty input dictionary
     if not waypoint_name_to_coordinates:
         return False, None
--- tests/test_waypoints_dict_to_list.py
+++ tests/test_waypoints_dict_to_list.py
@@ -1,8 +1,9 @@
 """
-Test Process.
+Testing various formats of waypoints dictionary during conversion to list process.
 """
 
 from modules import waypoints_dict_to_list
+
 
 def test_valid_waypoint_dict():
     """
@@ -13,21 +14,18 @@ def test_valid_waypoint_dict():
     expected = [(43.4340501,-80.5789803), (43.4335758,-80.5775237), (43.4336672,-80.57839)]  #expected output
 
     # determine if action was successful
-    result, output = waypoints_dict_to_list.waypoints_dict_to_list(input)
+    result, actual = waypoints_dict_to_list.waypoints_dict_to_list(input)
 
     assert result
-    assert output == expected
+    assert actual == expected
+
 
 def test_empty_waypoint_dict():
-    """
-    Test empty dictionary of waypoints.
-    """
-
     input =  {}
     expected = None  # expected output 
 
     # determine if action was successful
-    result, output = waypoints_dict_to_list.waypoints_dict_to_list(input)
+    result, actual = waypoints_dict_to_list.waypoints_dict_to_list(input)
 
-    assert not result
-    assert output == expected
+    assert result is not None
+    assert actual == expected
