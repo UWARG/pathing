33dbe1f3749418a4c250c7e3a544371d1c0df577
eshaan-mehta <eshaanm.mehta@gmail.com>
2023-11-07 22:30:25 -0500

    fixed periods again after rebase

--- tests/test_waypoints_dict_to_list.py
+++ tests/test_waypoints_dict_to_list.py
@@ -7,12 +7,12 @@ from modules import waypoints_dict_to_list
 
 def test_valid_waypoint_dict():
     """
-    Test conversion to list for a valid dict
+    Test conversion to list for a valid dict.
     """
     input =  {"Alpha": (43.4340501,-80.5789803), "Bravo": (43.4335758,-80.5775237), "Charlie": (43.4336672,-80.57839)}
     expected = [(43.4340501,-80.5789803), (43.4335758,-80.5775237), (43.4336672,-80.57839)]
 
-    # Determine if action was successful.
+    # Determine if action was successful
     result, actual = waypoints_dict_to_list.waypoints_dict_to_list(input)
 
     assert result
@@ -21,11 +21,11 @@ def test_valid_waypoint_dict():
 
 def test_empty_waypoint_dict():
     """
-    Test conversion to list for an empty dict
+    Test conversion to list for an empty dict.
     """
     input = {}
 
-    # Determine if action was successful.
+    # Determine if action was successful
     result, actual = waypoints_dict_to_list.waypoints_dict_to_list(input)
 
     assert not result
