2b44439e1aaa9cf76203cd1034367011a7e737b5
Rosanne Zhu <110139243+roskzhu@users.noreply.github.com>
2023-11-23 02:56:45 -0500

    added github workflow (#29)
    * init commit
    
    * added print messages in run func
    
    * fixed location and waypoint retrieval
    
    * init commit
    
    * added print messages in run func
    
    * fixed location and waypoint retrieval
    
    * misc changes for merge
    
    * reverted prev change for merge
    
    * added new function under modules
    
    * fixed description
    
    * fixed spacing
    
    * fixed waypoint_tracking
    
    * fixed styling
    
    * fixed function descriptions
    
    * modified return status
    
    * added print messages for location
    
    * modified dronekit connection
    
    * changed method of counting in waypoint_trakcing
    
    * added github workflow
    
    * added workflow fix
    
    * removed skipped pytests
    
    * Update run-tests.yml
    
    * add zbar library to fix build error
    
    * updated tests to remove ci errors
    
    * remove zbar installation
    
    * revert changes
    
    * adjusted comments on tests
    
    * comment adjustment

--- tests/test_qr_input.py
+++ tests/test_qr_input.py
@@ -1,8 +1,6 @@
 """
-Test process.
+Tests functionality for qr_input.
 """
-
-import pytest
 
 from modules import qr_input
 
@@ -10,20 +8,12 @@ from modules import qr_input
 CAMERA = 0
 
 
-pytest.skip("Integration test", allow_module_level=True)
-
-
-def test_qr_input():
-    """
-    Tests functionality for qr_input
-    """
-    is_qr_found, qr_string = qr_input.qr_input(CAMERA)
-
-    if is_qr_found:
-        print(f"Decoded QR code with string value: {qr_string}")
-    else:
-        print("Exited early before a QR code was read")
-
-
 if __name__ == '__main__':
-    test_qr_input()
+
+    result, qr_string = qr_input.qr_input(CAMERA)
+    assert result
+    assert qr_string is not None
+
+    print(f"Decoded QR code with string value: {qr_string}")
+
+    print("Done!")
