a4c807591fc1c9ae9abceba143dace1285ada81c
eshaan-mehta <eshaanm.mehta@gmail.com>
2023-11-25 19:05:37 -0500

    deduplicated waypoints

--- tests/test_waypoints_dict_to_list.py
+++ tests/test_waypoints_dict_to_list.py
@@ -2,25 +2,21 @@
 Testing various formats of waypoints dictionary during conversion to list process.
 """
 
-from modules import waypoints_dict_to_list
 from modules import waypoint
+from modules import waypoints_dict_to_list
 
 
 def test_valid_waypoint_dict():
     """
     Test conversion to list for a valid dict.
     """
-    waypoint_mapping = {
-        "Alpha": waypoint.Waypoint("Alpha", 43.4340501,-80.5789803),
-        "Bravo": waypoint.Waypoint("Bravo", 43.4335758,-80.5775237),
-        "Charlie": waypoint.Waypoint("Charlie", 43.4336672,-80.57839),
-    }
+    alpha = waypoint.Waypoint("Alpha", 43.4340501,-80.5789803)
+    bravo = waypoint.Waypoint("Bravo", 43.4335758,-80.5775237)
+    charlie = waypoint.Waypoint("Charlie", 43.4336672,-80.57839)
 
-    expected = [
-        waypoint.Waypoint("Alpha", 43.4340501,-80.5789803), 
-        waypoint.Waypoint("Bravo", 43.4335758,-80.5775237),
-        waypoint.Waypoint("Charlie", 43.4336672,-80.57839),
-    ]
+    waypoint_mapping = {"Alpha": alpha, "Bravo": bravo, "Charlie": charlie}
+
+    expected = [alpha, bravo, charlie]
 
     # Determine if action was successful
     result, actual = waypoints_dict_to_list.waypoints_dict_to_list(waypoint_mapping)
