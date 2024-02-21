0d8e3a1737a28b40340eeb06c6b9411f2069edfe
Jane <janezeng45@gmail.com>
2024-02-20 22:04:23 -0500

    more nits

--- path_2024_task_1.py
+++ path_2024_task_1.py
@@ -10,9 +10,19 @@ CONNECTION_ADDRESS = "tcp:localhost:14550"
 
 def run() -> int:
     """
-    Reads in hardcoded waypoints from CSV file and sends drone commands.
+    Uploads mission to run a maximum number of laps and monitors the mission for early landing.
     """
     # Wait ready is false as the drone may be on the ground
     drone = dronekit.connect(CONNECTION_ADDRESS, wait_ready = False)
-    
+
     return 0
+
+
+if __name__ == "__main__":
+    # Not a constant
+    # pylint: disable-next=invalid-name
+    result_run = run()
+    if result_run < 0:
+        print("ERROR")
+
+    print("Done")
