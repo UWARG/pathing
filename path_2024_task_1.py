e974284a3e7dc3dea5a98ac7d8f102a73e97dd65
Jane <97042818+janez45@users.noreply.github.com>
2024-02-20 22:21:01 -0500

    Created path_2024_task_1.py (#39)
    * Created path_2024_task_1.py
    
    * fix nits
    
    * more nits
    
    * comments and newline at the bottom

---
+++ path_2024_task_1.py
@@ -0,0 +1,30 @@
+"""
+Task 1 path.
+"""
+
+import dronekit
+
+
+CONNECTION_ADDRESS = "tcp:localhost:14550"
+
+
+# Required for checks
+# pylint: disable-next=too-many-return-statements
+def run() -> int:
+    """
+    Uploads mission to run a maximum number of laps and monitors the mission for early landing.
+    """
+    # Wait ready is false as the drone may be on the ground
+    drone = dronekit.connect(CONNECTION_ADDRESS, wait_ready = False)
+
+    return 0
+
+
+if __name__ == "__main__":
+    # Not a constant
+    # pylint: disable-next=invalid-name
+    result_run = run()
+    if result_run < 0:
+        print("ERROR")
+
+    print("Done")
