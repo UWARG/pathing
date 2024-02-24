cfa3814ac0d819507c4901528a04d34b470f1c41
Balaji <67008243+BalajiLeninrajan@users.noreply.github.com>
2024-02-23 22:37:11 -0500

    Create waypoint dictionary for Alpha (#41)
    * Created CSV for waypoint Alpha; Added conversion of waypoint Alpha to dictionary in main task_1; Added conversion of waypoint Alpha dictionary to list in main task_1
    
    * Renamed waypoint_alpha_test_1.csv to waypoint_task_1.csv
    
    * Removed references to alpha and fixed code formatting to follow guide
    
    * Removed extra decimal places from alpha's latitude in waypoint_task_1.csv
    
    * Capitalized error messages
    
    ---------
    
    Co-authored-by: Balaji Leninrajan <bleninrajan@uwaterloo.ca>

--- path_2024_task_1.py
+++ path_2024_task_1.py
@@ -1,10 +1,15 @@
 """
 Task 1 path.
 """
+import pathlib
 
 import dronekit
 
+from modules import load_waypoint_name_to_coordinates_map
+from modules import waypoints_dict_to_list
 
+
+WAYPOINT_FILE_PATH = pathlib.Path("2024", "waypoints", "waypoint_task_1.csv")
 CONNECTION_ADDRESS = "tcp:localhost:14550"
 
 
@@ -15,7 +20,25 @@ def run() -> int:
     Uploads mission to run a maximum number of laps and monitors the mission for early landing.
     """
     # Wait ready is false as the drone may be on the ground
-    drone = dronekit.connect(CONNECTION_ADDRESS, wait_ready = False)
+    drone = dronekit.connect(CONNECTION_ADDRESS, wait_ready=False)
+
+    # Create waypoint name to coordinate dictionary
+    result, waypoint_dictionary = \
+        load_waypoint_name_to_coordinates_map.load_waypoint_name_to_coordinates_map(
+            WAYPOINT_FILE_PATH,
+        )
+    if not result:
+        print("ERROR: Load waypoint to coordinates map")
+        return -1
+
+    # Convert waypoint dictionary to list
+    result, waypoint_list = \
+        waypoints_dict_to_list.waypoints_dict_to_list(
+            waypoint_dictionary,
+        )
+    if not result:
+        print("ERROR: Convert waypoint dictionary to list")
+        return -1
 
     return 0
 
