5cceba11db3d83293ae07c9ae683e98103de23ca
Daniel Zhang <danielcrzhang@gmail.com>
2024-03-13 22:33:17 -0400

    Create RTL module

--- modules/force_rtl.py
+++ modules/force_rtl.py
@@ -10,7 +10,7 @@ MAVLINK_RTL_FRAME = dronekit.mavutil.mavlink.MAV_FRAME_GLOBAL
 MAVLINK_RTL_COMMAND = dronekit.mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH
 
 
-def force_rtl(drone: dronekit.Vehicle)->None:
+def force_rtl(drone: dronekit.Vehicle) -> None:
     """
     Clear current drone mission, sends RTL command using upload_command module
     
@@ -24,6 +24,7 @@ def force_rtl(drone: dronekit.Vehicle)->None:
     None
     """
 
+    # clears current mission in a command
     rtl_command = dronekit.Command(
         0,
         0,
@@ -40,9 +41,10 @@ def force_rtl(drone: dronekit.Vehicle)->None:
         0,
         0,
     )
-
-    # upload RTL command
+    
+    # utilize upload_command function to give mission command to drone
     success = upload_commands.upload_commands(drone, [rtl_command])
+    
     # error if unsuccessful
     if not success:
         print("Unable to upload RTL command to drone command sequence.")
