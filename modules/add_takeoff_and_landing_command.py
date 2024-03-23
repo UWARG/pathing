dd5c9136f5d291932215c8b25d44667b3f695a38
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-03-22 21:32:30 -0400

    Changed None to 0

--- modules/add_takeoff_and_landing_command.py
+++ modules/add_takeoff_and_landing_command.py
@@ -62,7 +62,7 @@ def add_takeoff_and_landing_command(commands: "list[dronekit.Command]",
         0,  # param1
         PERCISION_LAND_MODE,
         0,
-        None,
+        0,
         0,
         0,
         0,
