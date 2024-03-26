42f2aad71b8f20a26a9a265de35427dfe03d920c
Jane <janezeng45@gmail.com>
2024-03-26 18:52:30 -0400

    recreating of part 5 after all the formatters

---
+++ modules/create_mission_from_waypoints.py
@@ -0,0 +1,53 @@
+"""
+Function that appends waypoints for N laps to create a mission
+"""
+
+import dronekit
+
+from . import waypoints_to_commands
+from .common.kml.modules import location_ground
+
+
+def create_mission_from_waypoints(
+    num_laps: int,
+    takeoff_altitude: int,
+    laps_altitude: int,
+    start_sequence_list: "list[location_ground.LocationGround]",
+    lap_sequence_list: "list[location_ground.LocationGround]",
+) -> "tuple[bool, list[dronekit.Command] | None]":
+    """
+    Creates a mission(list of dronekit commands) from starting sequence waypoints
+    and lap sequence waypoints repeated N times
+
+    Parameters:
+        - num_laps (int): The number of laps to run
+        - takeoff_altitude (int): Drone's altitude in metres for takeoff
+        - laps_altitude (int): Drone's altitude in metres for laps
+        - start_seqeunce_list (list[LocationGround]): A list of waypoints for the starting sequence
+        - lap_sequence_list (list[LocationGround]): A list of waypoints representing one lap
+
+    Returns:
+        - (False, None): If there is an error in converting any waypoints into commands
+        - (True, mission_waypoints_commands): A list of dronekit commands that represent the mission
+    """
+    # convert the starting sequence into a list of commands
+    success, takeoff_commands = waypoints_to_commands.waypoints_to_commands(
+        start_sequence_list, takeoff_altitude
+    )
+    if not success:
+        return False, None
+
+    # Append the lap sequence n times
+    laps_list = []
+    for _ in range(num_laps):
+        laps_list += lap_sequence_list
+
+    # Convert the laps list into a list of commands
+    success, laps_commands = waypoints_to_commands.waypoints_to_commands(laps_list, laps_altitude)
+    if not success:
+        return False, None
+
+    # Create and return the mission dronekit commands
+    mission_waypoints_commands = takeoff_commands + laps_commands
+
+    return True, mission_waypoints_commands
---
+++ tests/unit/test_create_mission_from_waypoints.py
@@ -0,0 +1,119 @@
+"""
+Test process for the create_mission_from_waypoints module
+"""
+
+import pytest
+
+from modules import create_mission_from_waypoints
+from modules.common.kml.modules import location_ground
+
+
+TAKEOFF_ALTITUDE = 100
+LAPS_ALTITUDE = 50
+NUM_LAPS = 2
+
+
+@pytest.fixture
+def non_empty_start_sequence() -> "list[location_ground.LocationGround]":
+    takeoff_sequence = [
+        location_ground.LocationGround("Alpha", 48.5112750, -71.6505486),
+    ]
+    yield takeoff_sequence
+
+
+@pytest.fixture
+def non_empty_lap_sequence() -> "list[location_ground.LocationGround]":
+    lap_sequence = [
+        location_ground.LocationGround("waypoint 1", 43.4363951, -80.5861617),
+        location_ground.LocationGround("waypoint 2", 43.4335848, -80.5767288),
+        location_ground.LocationGround("waypoint 3", 43.4338880, -80.5764634),
+        location_ground.LocationGround("waypoint 4", 43.4366668, -80.5859371),
+    ]
+    yield lap_sequence
+
+
+def test_no_laps(
+    non_empty_start_sequence: "list[location_ground.LocationGround]",
+    non_empty_lap_sequence: "list[location_ground.LocationGround]",
+) -> None:
+    """
+    Tests for correct functionality when we pass in no laps
+    """
+    success, mission = create_mission_from_waypoints.create_mission_from_waypoints(
+        0,
+        TAKEOFF_ALTITUDE,
+        LAPS_ALTITUDE,
+        non_empty_start_sequence,
+        non_empty_lap_sequence,
+    )
+    assert not success
+    assert mission is None
+
+
+def test_empty_start_sequence(
+    non_empty_lap_sequence: "list[location_ground.LocationGround]",
+) -> None:
+    """
+    Tests functionaltiy for an empty start sequence
+    """
+    start_sequence = []
+    success, mission = create_mission_from_waypoints.create_mission_from_waypoints(
+        NUM_LAPS,
+        TAKEOFF_ALTITUDE,
+        LAPS_ALTITUDE,
+        start_sequence,
+        non_empty_lap_sequence,
+    )
+    assert not success
+    assert mission is None
+
+
+def test_empty_lap_sequence(
+    non_empty_start_sequence: "list[location_ground.LocationGround]",
+) -> None:
+    """
+    Tests functionaltiy for an empty lap sequence
+    """
+    lap_sequence = []
+    success, mission = create_mission_from_waypoints.create_mission_from_waypoints(
+        NUM_LAPS,
+        TAKEOFF_ALTITUDE,
+        LAPS_ALTITUDE,
+        non_empty_start_sequence,
+        lap_sequence,
+    )
+    assert not success
+    assert mission is None
+
+
+def test_valid_waypoints(
+    non_empty_start_sequence: "list[location_ground.LocationGround]",
+    non_empty_lap_sequence: "list[location_ground.LocationGround]",
+) -> None:
+    """
+    Ensures that mission is correctly made based on valid parameters
+    """
+    print("Todo")
+    success, mission_actual = create_mission_from_waypoints.create_mission_from_waypoints(
+        NUM_LAPS,
+        TAKEOFF_ALTITUDE,
+        LAPS_ALTITUDE,
+        non_empty_start_sequence,
+        non_empty_lap_sequence,
+    )
+    assert success
+    assert isinstance(mission_actual, list)
+    assert len(mission_actual) == (
+        len(non_empty_start_sequence) + NUM_LAPS * len(non_empty_lap_sequence)
+    )
+
+    # list of the expected waypoints, making sure they mapped correctly
+    expected_list = non_empty_start_sequence + NUM_LAPS * non_empty_lap_sequence
+    for index, waypoint_command in enumerate(mission_actual):
+        if index < len(non_empty_start_sequence):
+            assert waypoint_command.z == TAKEOFF_ALTITUDE
+        else:
+            assert waypoint_command.z == LAPS_ALTITUDE
+
+        assert waypoint_command.x == expected_list[index].latitude
+        assert waypoint_command.y == expected_list[index].longitude
