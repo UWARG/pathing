ca2b78aced9004ed95f8ec8f1f7616c5bf0295a2
Jane <janezeng45@gmail.com>
2024-04-06 23:13:16 -0400

    update, moved module to task 1

---
+++ modules/check_stop_condition.py
@@ -0,0 +1,43 @@
+"""
+Checks whether the drone has reached its max flight time and sends it back to launch.
+"""
+
+import dronekit
+
+from . import generate_command
+from . import upload_commands
+
+
+DRONE_TIMEOUT = 30.0  # seconds
+
+
+def check_stop_condition(
+    start_time: float, current_time: float, drone: dronekit.Vehicle, maximum_flight_time: float
+) -> bool:
+    """
+    Check if drone exceeds the maximum flight time limit and replace with new mission of returning to launch.
+    Parameters
+    -----------
+    start_time: float
+        The time the drone started the mission in seconds.
+    current_time: float
+        Time elapsed in seconds since starting the mission.
+    drone: dronekit.Vehicle
+        The connected drone.
+    maximum_flight_time: float
+        Max flight time for drone in seconds.
+    Returns
+    -------
+    bool: True if max flight time was exceeded, False otherwise.
+    """
+    if current_time - start_time < maximum_flight_time:
+        return False
+
+    rtl_command = generate_command.return_to_launch()
+
+    # Invoke upload_commands to clear previous commands and direct drone back to launch location
+    result = upload_commands.upload_commands(drone, [rtl_command], DRONE_TIMEOUT)
+    if not result:
+        print("Unable to upload RTL command to drone command sequence.")
+
+    return True
--- path_2024.py
+++ path_2024.py
@@ -8,9 +8,8 @@ import time
 import dronekit
 
 from modules import add_takeoff_and_landing_command
-from modules import condition_evaluator
+from modules import check_stop_condition
 from modules import load_waypoint_name_to_coordinates_map
-from modules import mission_time_condition
 from modules import upload_commands
 from modules import waypoints_to_commands
 from modules import waypoint_tracking
@@ -88,16 +87,6 @@ def main() -> int:
         return -1
 
     start_time = time.time()
-    result, time_condition = mission_time_condition.MissionTimeCondition.create(
-        start_time, MAXIMUM_FLIGHT_TIME
-    )
-
-    if not result:
-        print("Error: Mission time condition")
-        return -1
-
-    return_to_launch_evaluator = condition_evaluator.ConditionEvaluator([time_condition])
-
     while True:
         result, waypoint_info = waypoint_tracking.get_current_waypoint_info(drone)
         if not result:
@@ -112,12 +101,14 @@ def main() -> int:
             print(f"Current location (Lat, Lon): {location}")
 
         # Send drone back to launch if exceeds time limit
-
-        should_return_to_launch = return_to_launch_evaluator.evaluate_all_conditions()
-        if should_return_to_launch:
+        current_time = time.time()
+        is_returning_to_launch = check_stop_condition.check_stop_condition(
+            start_time, current_time, drone, MAXIMUM_FLIGHT_TIME
+        )
+        if is_returning_to_launch:
             break
 
-        time_condition.output_time_elapsed()
+        print(f"Elapsed time (s): {current_time - start_time}")
 
         time.sleep(DELAY)
 
