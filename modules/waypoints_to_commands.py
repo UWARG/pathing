b89273c208fb5e13fcfed4bf210aba5d8ede6d4c
mgupta27 <mihir_gupta@rocketmail.com>
2024-04-14 22:52:39 -0400

    Created waypoint to command function with LocationGroundAndAltitude

--- modules/waypoints_to_commands.py
+++ modules/waypoints_to_commands.py
@@ -4,7 +4,7 @@ Function to convert list of waypoints to dronekit commands.
 
 import dronekit
 
-from . import generate_command
+from . import generate_command, location_ground_and_altitude
 from .common.kml.modules import location_ground
 
 
@@ -20,7 +20,7 @@ def waypoints_to_commands(
     Parameters
     ----------
     waypoints: list[LocationGround]
-        list of locationGround objects containing names and coordinates in decimal degrees.
+        list of LocationGround objects containing names and coordinates in decimal degrees.
     altitude: float
         altitude in meters to command the drone to.
 
@@ -38,6 +38,37 @@ def waypoints_to_commands(
     for point in waypoints:
         command = generate_command.waypoint(
             0.0, ACCEPT_RADIUS, point.latitude, point.longitude, altitude
+        )
+
+        dronekit_command_list.append(command)
+
+    return True, dronekit_command_list
+
+def waypoints_with_altitude_to_commands(
+    waypoints: "list[location_ground_and_altitude.LocationGroundAndAltitude]",
+) -> "tuple[bool, list[dronekit.Command] | None]":
+    """
+    Convert list of waypoints to dronekit commands.
+
+    Parameters
+    ----------
+    waypoints: list[LocationGroundAndAltitude]
+        list of LocationGroundAndAltitude objects containing names, coordinates in decimal degrees, and altitude in metres.
+
+    Returns
+    -------
+    tuple[bool, list[dronekit.Command] | None]:
+        (False, None) if empty waypoints list,
+        (True, dronekit commands that can be sent to the drone) otherwise.
+    """
+    if len(waypoints) == 0:
+        return False, None
+    
+    dronekit_command_list = []
+
+    for point in waypoints:
+        command = generate_command.waypoint(
+            0.0, ACCEPT_RADIUS, point.location_ground.latitude, point.location_ground.longitude, point.altitude
         )
 
         dronekit_command_list.append(command)
--- tests/unit/test_waypoints_to_commands.py
+++ tests/unit/test_waypoints_to_commands.py
@@ -4,7 +4,7 @@ Test process.
 
 import dronekit
 
-from modules import waypoints_to_commands
+from modules import waypoints_to_commands, location_ground_and_altitude
 from modules.common.kml.modules import location_ground
 
 
@@ -21,6 +21,11 @@ def test_waypoints_to_commands_empty_input() -> None:
     altitude = 100
 
     result, commands_actual = waypoints_to_commands.waypoints_to_commands(waypoints, altitude)
+
+    assert not result
+    assert commands_actual is None
+
+    result, commands_actual = waypoints_to_commands.waypoints_with_altitude_to_commands(waypoints)
 
     assert not result
     assert commands_actual is None
@@ -58,3 +63,36 @@ def test_waypoints_to_commands() -> None:
         assert command.x == lat_expected
         assert command.y == lng_expected
         assert command.z == altitude
+
+def test_waypoints_with_altitude_to_commands() -> None:
+    """
+    Tests functionality correctness of waypoints_with_altitude_to commands.
+    """
+    waypoints = [
+        location_ground_and_altitude.LocationGroundAndAltitude("Waypoint 1", 42.123, -73.456, 10.0),
+        location_ground_and_altitude.LocationGroundAndAltitude("Waypoint 2", 42.789, -73.987, 20.0),
+        location_ground_and_altitude.LocationGroundAndAltitude("Waypoint 3", 42.555, -73.321, 30.0),
+    ]
+
+    result, commands_actual = waypoints_to_commands.waypoints_with_altitude_to_commands(waypoints)
+
+    assert result
+
+    assert isinstance(commands_actual, list)
+    assert len(commands_actual) == len(waypoints)
+
+    for i, command in enumerate(commands_actual):
+        lat_expected = waypoints[i].location_ground.latitude
+        lng_expected = waypoints[i].location_ground.longitude
+        alt_expected = waypoints[i].altitude
+
+        assert isinstance(command, dronekit.Command)
+        assert command.frame == dronekit.mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT
+        assert command.command == dronekit.mavutil.mavlink.MAV_CMD_NAV_WAYPOINT
+        assert command.param1 == 0
+        assert command.param2 == waypoints_to_commands.ACCEPT_RADIUS
+        assert command.param3 == 0
+        assert command.param4 == 0
+        assert command.x == lat_expected
+        assert command.y == lng_expected
+        assert command.z == alt_expected
