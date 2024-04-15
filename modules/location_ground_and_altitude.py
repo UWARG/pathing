072fd70d9a60ddbec3e4c7b8933bb9c4b09737bd
mgupta27 <mihir_gupta@rocketmail.com>
2024-04-14 20:35:55 -0400

    Fixed formatting issues

--- modules/location_ground_and_altitude.py
+++ modules/location_ground_and_altitude.py
@@ -4,11 +4,12 @@ Class to store LocationGround and corresponding altitude.
 
 from .common.kml.modules import location_ground
 
+
 class LocationGroundAndAltitude:
     """
     LocationGroundAndAltitude represents a geographical ground location and an altitude
     with a name, latitude, longitude, and altitude.
-    
+
     Attributes:
         name (str): The name or label for the location.
         latitude (float): The latitude coordinate in decimal degrees.
@@ -22,36 +23,32 @@ class LocationGroundAndAltitude:
     """
 
     def __init__(self, name: str, latitude: float, longitude: float, altitude: float):
-       """
-       Constructor for the LocationGroundAndAltitude object.
+        """
+        Constructor for the LocationGroundAndAltitude object.
 
-       Args:
-           name (str): The name or label for the location.
-           latitude (float): The latitude coordinate in decimal degrees.
-           longitude (float): The longitude coordinate in decimal degrees.
-           altitude (float): The altitude of the coordinate in metres. 
-       """ 
-       self.location_ground = location_ground.LocationGround(name, latitude, longitude)
-       self.altitude = altitude
+        Args:
+            name (str): The name or label for the location.
+            latitude (float): The latitude coordinate in decimal degrees.
+            longitude (float): The longitude coordinate in decimal degrees.
+            altitude (float): The altitude of the coordinate in metres.
+        """
+        self.location_ground = location_ground.LocationGround(name, latitude, longitude)
+        self.altitude = altitude
 
-       def __eq__(self, other: "LocationGroundAndAltitude") -> bool:
-           """
-           Checks if two LocationGroundAndAltitude objects are equal.
+        def __eq__(self, other: "LocationGroundAndAltitude") -> bool:
+            """
+            Checks if two LocationGroundAndAltitude objects are equal.
 
-           Args:
-            other (LocationGroundAndAltitude): The other LocationGroundAndAltitude object to compare to.
-           """
-           if not isinstance(other, LocationGroundAndAltitude):
-               return False
-           
-           return(
-               self.location_ground == other.location_ground
-               and self.altitude == other.altitude
-           )
+            Args:
+             other (LocationGroundAndAltitude): The other LocationGroundAndAltitude object to compare to.
+            """
+            if not isinstance(other, LocationGroundAndAltitude):
+                return False
+
+            return self.location_ground == other.location_ground and self.altitude == other.altitude
 
     def __repr__(self) -> str:
         """
         String representation
         """
-        return \
-            f"{repr(self.location_ground)}, altitude: {self.altitude}" 
+        return f"{repr(self.location_ground)}, altitude: {self.altitude}"
