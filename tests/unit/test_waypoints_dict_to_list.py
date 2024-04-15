b23f35f3270039765acbc3fb5fe1a419c45ea6a9
mgupta27 <mihir_gupta@rocketmail.com>
2024-04-14 22:33:02 -0400

    Created overloaded dict to list function

--- tests/unit/test_waypoints_dict_to_list.py
+++ tests/unit/test_waypoints_dict_to_list.py
@@ -2,7 +2,7 @@
 Testing various formats of waypoints dictionary during conversion to list process.
 """
 
-from modules import waypoints_dict_to_list
+from modules import waypoints_dict_to_list, location_ground_and_altitude
 from modules.common.kml.modules import location_ground
 
 
@@ -21,6 +21,19 @@ def test_valid_waypoint_dict() -> None:
 
     waypoint_mapping = {"Alpha": alpha, "Bravo": bravo, "Charlie": charlie}
     expected = [alpha, bravo, charlie]
+
+    # Determine if action was successful
+    result, actual = waypoints_dict_to_list.waypoints_dict_to_list(waypoint_mapping)
+
+    assert result
+    assert actual == expected
+
+    delta = location_ground_and_altitude.LocationGroundAndAltitude("Delta", 43.4340501, -80.5789803, 10.0)
+    echo = location_ground_and_altitude.LocationGroundAndAltitude("Echo", 43.4335758, -80.5775237, 10.0)
+    golf = location_ground_and_altitude.LocationGroundAndAltitude("Golf", 43.4336672, -80.57839, 10.0)
+
+    waypoint_mapping = {"Delta": delta, "Echo": echo, "Golf": golf}
+    expected = [delta, echo, golf]
 
     # Determine if action was successful
     result, actual = waypoints_dict_to_list.waypoints_dict_to_list(waypoint_mapping)
