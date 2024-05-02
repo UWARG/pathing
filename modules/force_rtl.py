4a5dde45576d6489d714375e279e31baf62bd170
Daniel Zhang <danielcrzhang@gmail.com>
2024-05-02 19:20:49 -0400

    updated force_rtl.py to be used with generate_command module + formatting

--- modules/force_rtl.py
+++ modules/force_rtl.py
@@ -5,6 +5,7 @@ Forces drone to return to launch (RTL).
 import dronekit
 
 from . import upload_commands
+from . import generate_command
 
 MAVLINK_RTL_FRAME = dronekit.mavutil.mavlink.MAV_FRAME_GLOBAL
 MAVLINK_RTL_COMMAND = dronekit.mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH
@@ -24,23 +25,11 @@ def force_rtl(drone: dronekit.Vehicle) -> bool:
     bool: True if uploading RTL command is successful, False otherwise.
     """
 
-    # clears current mission
-    rtl_command = dronekit.Command(
-        0,
-        0,
-        0,
-        MAVLINK_RTL_FRAME,
-        MAVLINK_RTL_COMMAND,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-    )
+    # generate and set RTL command
+    rtl_command = generate_command.return_to_launch()
+    
+    # change drone mode
+    drone.mode = dronekit.VehicleMode("RTL")
     
     # utilize upload_command function to give RTL command to drone
     result = upload_commands.upload_commands(drone, [rtl_command])
