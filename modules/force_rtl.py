9715f2dc229254560b181fb5aaa68a5237440fb9
Daniel Zhang <danielcrzhang@gmail.com>
2024-05-02 19:57:43 -0400

    fix linting issue

--- modules/force_rtl.py
+++ modules/force_rtl.py
@@ -10,10 +10,11 @@ from . import generate_command
 MAVLINK_RTL_FRAME = dronekit.mavutil.mavlink.MAV_FRAME_GLOBAL
 MAVLINK_RTL_COMMAND = dronekit.mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH
 
+
 def force_rtl(drone: dronekit.Vehicle) -> bool:
     """
     Sends RTL command using the upload_command module.
-    
+
     Parameters
     -----------
     drone: dronekit.Vehicle
@@ -26,15 +27,15 @@ def force_rtl(drone: dronekit.Vehicle) -> bool:
 
     # generate and set RTL command
     rtl_command = generate_command.return_to_launch()
-    
+
     # change drone mode
     drone.mode = dronekit.VehicleMode("RTL")
-    
+
     # utilize upload_command function to give RTL command to drone
     result = upload_commands.upload_commands(drone, [rtl_command])
-    
+
     # error if unsuccessful
     if not result:
         print("Unable to upload RTL command to drone command sequence.")
-        
+
     return result
