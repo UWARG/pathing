e67e542711be4efb3396146e0a9b520a0518c482
Pranav <57974336+officialpranav@users.noreply.github.com>
2024-05-30 02:36:38 -0400

    fix bug in greedy algorithm, where range consumed is not accurate

--- generate_flight_plan.py
+++ generate_flight_plan.py
@@ -6,22 +6,17 @@ import math
 import pathlib
 from modules import waypoint
 
-import random
+import random #to generate test cases, remove in production
 
 WAYPOINT_FILE_PATH = pathlib.Path("waypoints", "wrestrc_waypoints.csv")
 RESULT, WAYPOINT_NAMES_TO_COORDINATES = load_waypoint_name_to_coordinates_map.load_waypoint_name_to_coordinates_map(WAYPOINT_FILE_PATH)
 
 EARTH_RADIUS = 6371
 
-TIME_LIMIT = 30
 PASSENGER_CAPACITY = 6
-DRONE_WEIGHT = 8
-DRONE_RANGE = 1000
-BEGINNING_WAYPOINT = "Alpha"
-
-example_qr_text = "Route number 1: 2 pers; Lima; Quebec; 15 kg; obstacle 2 m to NE; $112 Route number 2: 6 pers; Delta; Charlie; 5 kg; nil; $50 Route number 3: 4 pers; Alpha; Zulu; 15 kg; other comment; $150"
-
-example_qr_text_2 = "Route number 1: 4 pers; Charlie; Golf; 2 kg; nil; $159 Route number 2: 2 pers; Yankee; November; 6 kg; nil; $77 Route number 3: 6 pers; India; Echo; 4 kg; nil; $116 Route number 4: 5 pers; Oscar; Papa; 7 kg; nil; $124 Route number 5: 2 pers; Hotel; Delta; 7 kg; nil; $190 Route number 6: 2 pers; Golf; Delta; 4 kg; nil; $63 Route number 7: 2 pers; Foxtrot; Juliette; 6 kg; nil; $188 Route number 8: 4 pers; Alpha; Romeo; 8 kg; nil; $57 Route number 9: 2 pers; Tango; Sierra; 1 kg; nil; $52 Route number 10: 6 pers; Golf; Lima; 9 kg; nil; $123 Route number 11: 3 pers; Kilo; Point 18; 14 kg; nil; $106 Route number 12: 4 pers; Quebec; Zulu; 15 kg; nil; $143 Route number 13: 4 pers; Golf; India; 12 kg; nil; $71 Route number 14: 5 pers; Romeo; Juliette; 12 kg; nil; $62 Route number 15: 3 pers; Sierra; Oscar; 12 kg; nil; $151 Route number 16: 4 pers; Xray; November; 9 kg; nil; $56 Route number 17: 3 pers; Bravo; Alpha; 14 kg; nil; $69 Route number 18: 5 pers; Charlie; Whiskey; 2 kg; nil; $152 Route number 19: 3 pers; Whiskey; Papa; 5 kg; nil; $151 Route number 20: 1 pers; Romeo; Echo; 2 kg; nil; $73"
+DRONE_WEIGHT = 8 #kg
+DRONE_RANGE = 1000 #Battery recharges are included in range
+BEGINNING_WAYPOINT = "Alpha" #starting waypoint
 
 class Route:
   def __init__(self, route_number: int, passengers: int, start_waypoint: str, end_waypoint: str, max_weight: float, notes: str, profit: int):
@@ -52,7 +47,7 @@ def distance_between_coordinates(coordinate_1: waypoint.location_ground.Location
   
   return dist
   
-def process_qr_text(qr_text) -> List[Route]:
+def qr_text_to_routes(qr_text) -> List[Route]:
   #get list of all valid routes (removes routes that exceed passenger capacity or drone weight limit)
   
   route_pattern = r"Route number (\d+): (\d+) pers; ([^;]+); ([^;]+); (\d+) kg; ([^;]+); \$(\d+)"
@@ -84,27 +79,24 @@ def get_next_route(current_location: waypoint.location_ground.LocationGround, av
       total_distance = route.distance
     else:
       total_distance = distance_between_coordinates(current_location, WAYPOINT_NAMES_TO_COORDINATES[route.start_waypoint]) + route.distance
-    
     if(total_distance > remaining_range):
       continue
-    
     profit_per_km = route.profit/total_distance
     
     if(profit_per_km > highest_profit_per_km):
       highest_profit_per_km = profit_per_km
       best_route = route
-
-    range_consumed = total_distance
+      range_consumed = total_distance
     
   if(highest_profit_per_km == 0):
     return False, None, None
 
   return True, best_route, range_consumed
 
-def get_flight_plan(max_range: float, routes: List[Route]) -> List[Route]:
+def get_flight_plan(drone_range: float, routes: List[Route]) -> List[Route]:
   current_location = WAYPOINT_NAMES_TO_COORDINATES[BEGINNING_WAYPOINT]
   
-  remaining_range = max_range
+  remaining_range = drone_range
   
   #objectives: maximize earnings --> prioritize earnings/total distance
   routes_copy = copy.deepcopy(routes)
@@ -129,32 +121,35 @@ def generate_test_cases(number_of_cases: int):
   for i in range (number_of_cases):
     waypoint_names = list(WAYPOINT_NAMES_TO_COORDINATES.keys())
     passengers = random.randint(1, 6)
-    max_weight = random.randint(1, 15)
+    max_weight = random.randint(5, 20)
     profit = random.randint(50, 200)
 
 
-    start = random.choice(waypoint_names)
-    end = random.choice(waypoint_names)
+    start_waypoint = random.choice(waypoint_names)
+    end_waypoint = random.choice(waypoint_names)
 
-    while(start == end):
-      end = random.choice(waypoint_names)
+    while(start_waypoint == end_waypoint):
+      end_waypoint = random.choice(waypoint_names)
 
-    str = f"Route number {i + 1}: {passengers} pers; {start}; {end}; {max_weight} kg; nil; ${profit} "
+    route_text = f"Route number {i + 1}: {passengers} pers; {start_waypoint}; {end_waypoint}; {max_weight} kg; nil; ${profit} "
     
-    qr_text += str
+    qr_text += route_text
 
   return qr_text
 
-processed_text = process_qr_text(example_qr_text)
+NUM_TEST_CASES = 10
 
-flight_plan_result = get_flight_plan(DRONE_RANGE, processed_text)
+test_qr_text = generate_test_cases(NUM_TEST_CASES)
+routes_list = qr_text_to_routes(test_qr_text)
+flight_plan_result = get_flight_plan(DRONE_RANGE, routes_list)
+revenue = 0
 
-profit = 0
+print("Flight Plan:")
+for route_item in flight_plan_result:
+  revenue += route_item.profit
+  print(route_item)
 
-for route in flight_plan_result:
-  profit += route.profit
-  
-  
-  print(route)
+print("fraction of routes completed:", len(flight_plan_result)/NUM_TEST_CASES) #percentage of routes completed
+print(f"Total profit: ${revenue}") #total profit
 
-print(f"Total profit: ${profit}")
+# TODO: find a way to get the optimal flight plan. The current implementation is a greedy algorithm that may not always be optimal.
