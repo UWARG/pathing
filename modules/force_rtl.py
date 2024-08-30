89fa0d3fb85c7ba5bcb30ebabf3d2adc58307b51
Aaron <89286330+AaronWang04@users.noreply.github.com>
2024-08-29 23:43:09 -0400

    reverted waypoint_tracking, force_rtl bug fix
    waypoint tracking is not needed to be written in a function via flightcontroller, the logic is too identical to functions within flightcontroller so theres no need to add another wrapper.

--- modules/force_rtl.py
+++ modules/force_rtl.py
@@ -15,20 +15,26 @@ def force_rtl(controller: FlightController) -> bool:
     Parameters
     -----------
     controller: FlightController
-        The connected drone.
+        The connected drone's flightcontroller instance.
 
     Returns
     -------
     bool: True if uploading RTL command is successful, False otherwise.
     """
-
+    
     # Generate and set RTL command
     rtl_command = generate_command.return_to_launch()
+
+    # Change drone mode
+    result = controller.set_flight_mode("RTL")
+
+    if not result:
+        print("Unable to set drone mode to RTL.")
+        return result
 
     # Utilize upload_command function to give RTL command to drone
     result = controller.upload_commands([rtl_command])
 
-    # Error if unsuccessful
     if not result:
         print("Unable to upload RTL command to drone command sequence.")
 
