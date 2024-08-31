10ddae776bf08247b9b1ff53f49e1f224450d10d
ellyokes253 <tookoro@uwaterloo.ca>
2024-08-31 13:01:37 -0600

    adding FlightController library (#66)
    * adding FlightController library
    
    * fixed changes
    
    * trying to fix build
    
    * fixing formatting issues
    
    * fixing requested changes
    
    * refactor controller, fixed format
    
    * fixing changes for force_rtl
    
    * fixed changes for waypoint_tracker
    
    * fixing linter issues
    
    * fixing changes
    
    * reverted waypoint_tracking, force_rtl bug fix
    
    waypoint tracking is not needed to be written in a function via flightcontroller, the logic is too identical to functions within flightcontroller so theres no need to add another wrapper.
    
    * linter fix
    
    * Update force_rtl.py
    
    ---------
    
    Co-authored-by: Aaron <89286330+AaronWang04@users.noreply.github.com>

--- modules/force_rtl.py
+++ modules/force_rtl.py
@@ -2,24 +2,19 @@
 Forces drone to return to launch (RTL).
 """
 
-import dronekit
+from modules.common.mavlink.modules.flight_controller import FlightController
 
-from . import upload_commands
 from . import generate_command
 
-MAVLINK_RTL_FRAME = dronekit.mavutil.mavlink.MAV_FRAME_GLOBAL
-MAVLINK_RTL_COMMAND = dronekit.mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH
-DRONE_TIMEOUT = 30.0  # seconds
 
-
-def force_rtl(drone: dronekit.Vehicle) -> bool:
+def force_rtl(controller: FlightController) -> bool:
     """
     Sends RTL command using the upload_command module.
 
     Parameters
     -----------
-    drone: dronekit.Vehicle
-        The connected drone.
+    controller: FlightController
+        The connected drone's flightcontroller instance.
 
     Returns
     -------
@@ -30,12 +25,15 @@ def force_rtl(drone: dronekit.Vehicle) -> bool:
     rtl_command = generate_command.return_to_launch()
 
     # Change drone mode
-    drone.mode = dronekit.VehicleMode("RTL")
+    result = controller.set_flight_mode("RTL")
+
+    if not result:
+        print("Unable to set drone mode to RTL.")
+        return result
 
     # Utilize upload_command function to give RTL command to drone
-    result = upload_commands.upload_commands(drone, [rtl_command], DRONE_TIMEOUT)
+    result = controller.upload_commands([rtl_command])
 
-    # Error if unsuccessful
     if not result:
         print("Unable to upload RTL command to drone command sequence.")
 
