a9b3445cd191e645f6318e62ab30a6d50e13cbe9
luckyducky037 <leonardozhou2020@gmail.com>
2024-09-28 22:12:28 -0400

    Variable frustum

--- modules/search-dimensions/search_area_dimensions.py
+++ modules/search-dimensions/search_area_dimensions.py
@@ -11,18 +11,26 @@ FIELD_OF_VISION_Y = atan(18 * (13 / 23) / 2 / 15.125 * (10 / 16)) * 2
 
 def search_area_dimensions(
     height: int,
+    frustum_angle_x: float,
+    frustum_angle_y: float,
     field_of_vision_x: float = FIELD_OF_VISION_X,
     field_of_vision_y: float = FIELD_OF_VISION_Y,
 ) -> "tuple[float, float]":
     """
     Parameters:
         - height: height of the drone, in metres
+        - frustum_angle_x: the left-right camera direction angle w/ respect to vertical
+        - frustum_angle_y: the up-down camera direction angle w/ respect to vertical
         - field_of_vision_x: the horizontal field of vision of the camera, in radians (default to measured constant)
         - field_of_vision_y: the vertical field of vision of the camera, in radians (default to measured constant)
 
     Return:
         - tuple containing the rectangular dimensions of the field of view of the camera
     """
-    horizontal_distance = tan(field_of_vision_x / 2) * height * 2
-    vertical_distance = tan(field_of_vision_y / 2) * height * 2
+    left_distance = tan((field_of_vision_x - frustum_angle_x) / 2) * height
+    right_distance = tan((field_of_vision_x + frustum_angle_x) / 2) * height
+    horizontal_distance = left_distance + right_distance
+    up_distance = tan((field_of_vision_y - frustum_angle_y) / 2) * height
+    down_distance = tan((field_of_vision_y + frustum_angle_y) / 2) * height
+    vertical_distance = up_distance + down_distance
     return horizontal_distance, vertical_distance
