8f7804d261f8a96f2fe2eba6a4d06173187e35c7
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2024-11-03 13:34:01 -0500

    Refactor plot_circular_path.py

--- modules/plot_circular_path.py
+++ modules/plot_circular_path.py
@@ -2,13 +2,14 @@
 Module to plot n circular waypoints, given a center and radius.
 """
 
-import math
 import csv
+import math
 
-from .waypoint import Waypoint
 from modules.common.mavlink.modules.drone_odometry import DronePosition
 from modules.common.mavlink.modules.drone_odometry_local import DronePositionLocal
 from modules.common.mavlink.modules.local_global_conversion import drone_position_global_from_local
+
+from .waypoint import Waypoint
 
 
 def move_coordinates_by_offset(
@@ -29,24 +30,19 @@ def move_coordinates_by_offset(
         Waypoint: the resulting waypoint after being moved by offset from the
         original waypoint.
     """
-    offset_local = DronePositionLocal.create(offset_y, offset_x, 0)
+    _, offset_local = DronePositionLocal.create(offset_y, offset_x, 0)
 
     # Because drone_position_global_from_local requires DronePosition class,
     # we need to convert Waypoint to DronePosition first
-    start_lat = start_point.location_ground.latitude
-    start_lon = start_point.location_ground.longitude
-    start_alt = start_point.altitude
-    start_point_converted = DronePosition.create(start_lat, start_lon, start_alt)
-
-    success, end_point = drone_position_global_from_local(
-        start_point_converted, start_point, offset_local
+    _, start_point_converted = DronePosition.create(
+        start_point.location_ground.latitude,
+        start_point.location_ground.longitude,
+        start_point.altitude,
     )
 
-    end_lat = end_point.latitude
-    end_lon = end_point.longitude
-    end_alt = end_point.altitude
-    end_point_converted = Waypoint(name, end_lat, end_lon, end_alt)
-    return end_point_converted
+    _, end_point = drone_position_global_from_local(start_point_converted, offset_local)
+
+    return Waypoint(name, end_point.latitude, end_point.longitude, end_point.altitude)
 
 
 def generate_circular_path(center: Waypoint, radius: float, num_points: int) -> "list[Waypoint]":
