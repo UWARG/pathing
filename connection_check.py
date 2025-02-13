e3b6660d0c08a99edecdc80dff7c698eedc8e239
honzikschenk <nojschenk@gmail.com>
2025-02-13 18:12:28 -0500

    Refactor connection_check to use FlightController instead of DroneKit

--- connection_check.py
+++ connection_check.py
@@ -1,12 +1,10 @@
 """
-For testing MAVLink connection with DroneKit-Python.
+For testing MAVLink connection with FlightController.
 """
 
 import time
 
-from pymavlink import mavutil
-
-from modules.common.modules.mavlink import dronekit
+from modules.common.modules.mavlink import flight_controller
 
 
 # Set these to test what you want to test
@@ -19,106 +17,84 @@ CONNECTION_ADDRESS = "tcp:localhost:14550"
 ALTITUDE = 40  # metres
 
 
-def write_test_mission(drone: dronekit.Vehicle) -> bool:
+def write_test_mission(drone: flight_controller.FlightController) -> bool:
     """
     Creates and sends a hardcoded test mission to the drone.
     """
-    commands = []
 
-    # Create and add commands
-    command0 = dronekit.Command(
-        0,
-        0,
-        0,
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
-        0,
-        0,
-        0,  # param1
-        10,
-        0,
-        0,
-        -35.3632610,
-        149.1691446,
-        ALTITUDE,
-    )
+    result = drone.drone.commands.clear()
 
-    commands.append(command0)
+    if not result:
+        print("ERROR: Could not clear mission.")
+        return False
 
-    command1 = dronekit.Command(
-        0,
-        0,
-        0,
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
-        0,
-        0,
-        0,  # param1
-        10,
-        10,
-        0,
-        -35.3603736,
-        149.1689944,
-        ALTITUDE,
-    )
+    # Create and upload commands
+    result = drone.insert_waypoint(0, -35.3632610, 149.1691446, ALTITUDE)
 
-    commands.append(command1)
+    if not result:
+        print("ERROR: Could not upload command 0.")
+        return False
 
-    # Get the set of commands from the drone
-    command_sequence = drone.commands
-    command_sequence.download()
-    command_sequence.wait_ready()
-    command_sequence.clear()
-    for command in commands:
-        command_sequence.add(command)
+    result = drone.insert_waypoint(1, -35.3603736, 149.1689944, ALTITUDE)
 
-    # Upload commands to drone
-    command_sequence.upload()
+    if not result:
+        print("ERROR: Could not upload command 1.")
+        return False
 
     return True
 
 
-def read_data(drone: dronekit.Vehicle) -> bool:
+def read_data(drone: flight_controller.FlightController) -> bool:
     """
     Prints drone telemetry and mission status.
     """
+    result, odometry = drone.get_odometry()
+
+    if not result:
+        print("ERROR: Could not get odometry.")
+        return False
+
+    print("Odometry:")
+    print("Position:")
+
     print("------")
     print("Position:")
-    print(drone.location.global_frame.alt)
-    print(drone.location.global_frame.lat)
-    print(drone.location.global_frame.lon)
+    print(odometry.position.altitude)
+    print(odometry.position.latitude)
+    print(odometry.position.longitude)
     print("Attitude:")
-    print(drone.attitude.pitch)
-    print(drone.attitude.roll)
-    print(drone.attitude.yaw)
-
-    command_sequence = drone.commands
-
-    command_sequence.download()
-    command_sequence.wait_ready()
+    print(odometry.orientation.pitch)
+    print(odometry.orientation.roll)
+    print(odometry.orientation.yaw)
 
     print("Command information:")
-    print("Waypoint total count: " + str(command_sequence.count))
-    print("Next waypoint index: " + str(command_sequence.next))
+    print("Waypoint total count: " + str(drone.drone.commands.count))
+    print("Next waypoint index: " + str(drone.drone.commands.next))
+
+    return True
 
 
 def main() -> int:
     """
     Main function.
     """
-    # TODO: Fails when wait_ready=True, debugging why this is
-    # wait_ready=False is dangerous
-    dronekit_vehicle = dronekit.connect(CONNECTION_ADDRESS, wait_ready=False)
+    result, flight_controller_vehicle = flight_controller.FlightController.create(
+        CONNECTION_ADDRESS
+    )
+
+    if not result:
+        print("ERROR: Could not connect to the flight controller.")
+        return -1
 
     if WRITE_TEST:
-        write_test_mission(dronekit_vehicle)
+        write_test_mission(flight_controller_vehicle)
 
     if READ_TEST:
         for _ in range(0, 40):
-            read_data(dronekit_vehicle)
+            read_data(flight_controller_vehicle)
             time.sleep(0.5)
 
-    dronekit_vehicle.close()
+    flight_controller_vehicle.close()
 
     return 0
 
