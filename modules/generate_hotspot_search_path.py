2168810605e233ad3be6c6a94f5058ff64671bc7
Haokai Xuan <76743474+haokai-xuan@users.noreply.github.com>
2025-04-06 15:44:00 -0400

    array split implementation (#87)
    * array split implementation
    
    * added array flattening and unit tests
    
    * formatted code
    
    * fixed pylint issue
    
    * created generate_search_path wrapper

--- modules/generate_hotspot_search_path.py
+++ modules/generate_hotspot_search_path.py
@@ -12,11 +12,11 @@ MULTIPLIER = 1.2
 MINIMUM_POINTS = 3
 
 
-def generate_search_path(
+def generate_search_paths(
     center: position_global_relative_altitude.PositionGlobalRelativeAltitude,
     search_radius: float,
     search_area_dimensions: "tuple[float, float]",
-) -> "tuple[bool, list[position_global_relative_altitude.PositionGlobalRelativeAltitude] | None]":
+) -> "tuple[bool, list[list[position_global_relative_altitude.PositionGlobalRelativeAltitude]] | None]":
     """
     Generates list of spline waypoints representing concentric rings for drone search path.
 
@@ -24,7 +24,7 @@ def generate_search_path(
     search_radius: drone search radius.
     search_area_dimensions: search area width, height
 
-    Returns: Success, list of waypoints.
+    Returns: Success, list of list of waypoints.
     """
     camera_horizontal_size, camera_vertical_size = search_area_dimensions
 
@@ -53,8 +53,80 @@ def generate_search_path(
         )
         if not result or waypoints is None:
             return False, None
-        all_waypoints.extend(waypoints)
+        all_waypoints.append(waypoints)
 
         current_radius += camera_horizontal_size
 
     return True, all_waypoints
+
+
+def __flatten_waypoints(
+    waypoints: list[list[position_global_relative_altitude.PositionGlobalRelativeAltitude]],
+) -> list[position_global_relative_altitude.PositionGlobalRelativeAltitude]:
+    """
+    Private function that transforms the list of list of all waypoints by circle into a linear list.
+
+    waypoints: list of list of all waypoints by circle
+
+    Returns: Linear list of all the waypoints.
+    """
+
+    flattened_waypoints = [waypoint for circle in waypoints for waypoint in circle]
+
+    return flattened_waypoints
+
+
+def generate_search_path(
+    center: position_global_relative_altitude.PositionGlobalRelativeAltitude,
+    search_radius: float,
+    search_area_dimensions: "tuple[float, float]",
+) -> "tuple[bool, list[position_global_relative_altitude.PositionGlobalRelativeAltitude] | None]":
+    """
+    Wrapper function that generates list of spline waypoints representing concentric rings for drone search path.
+
+    center: waypoint for center of circle.
+    search_radius: drone search radius.
+    search_area_dimensions: search area width, height
+
+    Returns: Success, list of waypoints.
+    """
+    result, waypoints = generate_search_paths(center, search_radius, search_area_dimensions)
+    if not result:
+        return False, None
+    return True, __flatten_waypoints(waypoints)
+
+
+def get_search_path_sector(
+    waypoints: list[list[position_global_relative_altitude.PositionGlobalRelativeAltitude]],
+    total_drones: int,
+    drone_index: int,
+) -> list[position_global_relative_altitude.PositionGlobalRelativeAltitude]:
+    """
+    Generates a sector of concentric circle for corresponding drone index.
+
+    waypoints: list of all waypoints in concentric circles
+    total_drones: the total number of drones flying
+    drone_index: [0, num_drones)
+
+    Returns: list of waypoints in sector for specific drone
+    """
+
+    if drone_index >= total_drones or drone_index < 0:
+        print(f"ERROR: drone index must be in [0, total_drones): {drone_index}")
+        return []
+
+    sector_waypoints = []
+    number_of_circles = len(waypoints)
+
+    for i in range(number_of_circles):
+        waypoints_per_drone = len(waypoints[i]) // total_drones
+
+        start = drone_index * waypoints_per_drone
+
+        if drone_index == total_drones - 1:
+            waypoints_per_drone += len(waypoints[i]) % total_drones  # Last drone handles extras
+
+        end = start + waypoints_per_drone
+        sector_waypoints.extend(waypoints[i][start:end])
+
+    return sector_waypoints
